108c9032fa5ff0a284038780822e1011065f891c
Ahsap Officiel <karldimitri96@gmail.com>
2022-09-01 19:01:20 +0100

    models created

--- src/models/visa-operations.ts
+++ src/models/visa-operations.ts
@@ -1,111 +1,159 @@
-export interface VisaOperations {
-  _id?: string;
-  clientCode?: string;
-  userId?: string;
-  currentMonth?: number;
-  ncp?: string;
-  nbrTransactions?: {
-    tpew?: number; // Retrait et paiement tpe
-    onp?: number; // paiement en ligne
-  };
-  amounts?: {
-    tpew?: number; // Retrait et paiement tpe
-    onp?: number; // paiement en ligne
-  };
-  cards?: any[];
-  date?: {
-    created?: number;
-    updated?: number;
-  };
-  operationTypes?: OperationType[];
-  attachements?: Attachement[];
-  status?: OperationStatus; // 100: aucun upload, 101: au moins un upload, 200: upload complet, en cours, 201: accepté, 202: refusé
-  rejectReason?:string;
-}
-
 export interface Attachement {
-  code?: string; // files collection Id
-  status?: AttachementStatus;
-  label?: string;
-  type?: number; // 100:retrait GAB, 200: paiement tpe, 300:paiement en ligne
-  contentType?: string;
-  date?: {
-    created?: number;
-    updated?: number;
-  };
-  rejectReason?: string;
-  isRequired?: boolean;
-  extensions?: string;
+    extension?: any;
+    code?: string;
+    path?: string;
+    label?: string;
+    status?: AttachementStatus;
+    voucherId?: string;
+    contentType?: string;
+    content?: string;
+    dates?: {
+        created?: number;
+        updated?: number;
+    };
+    isRequired?: boolean;
+    rejectReason?: string;
 }
 
+export interface Voucher {
+    _id?: string;
+    label?: string;
+    extension?: string;
+    description?: string;
+    isRequired?: boolean;
+}
 
 export enum AttachementStatus {
-  UNAVAILABLE = 100, // When attachement is not uploaded
-  PENDING = 200, // When attachechement is uploaded but not changed
-  ACCEPTED = 300, // When attachement is accepted
-  REJECTED = 400, // When attachement is refused
+    UNAVAILABLE = 100, // When attachement is not uploaded
+    PENDING = 200, // When attachechement is uploaded but not changed
+    ACCEPTED = 300, // When attachement is accepted
+    REJECTED = 400, // When attachement is refused
 
 }
 
 export enum OperationType {
-  TPE = 100,
-  ONP = 200
+    ELECTRONIC_PAYMENT_TERMINAL = 100,
+    ATN_WITHDRAWAL = 200
 }
 export enum OperationStatus {
-  UNAVAILABLE = 100, // aucun upload,
-  AVAILABLE = 101, // au moins un upload,
-  PENDING = 200, //  upload complet , en cours
-  ACCEPTED = 201, // accepté
-  REJECTED = 202, //  refusé
+    UNAVAILABLE = 100, // aucun upload,
+    AVAILABLE = 101, // au moins un upload,
+    PENDING = 200, //  upload complet , en cours
+    ACCEPTED = 201, // accepté
+    REJECTED = 202, //  refusé
 }
-
-export interface FileAttachement {
-  label?: string;
-  extensions?: string;
-  isRequired?: boolean;
-}
-
-export interface SettingFile {
-  _id?: string;
-  label?: string; // type d'opération(retrait GAB & paiement ou paiement en ligne)
-  type?: number; // 100:retrait GAB & paiement tpe, 200:paiement en ligne
-  date?: {
-    created?: number;
-    updated?: number;
-  };
-  files?: FileAttachement[];
-}
-
 export interface VisaTransaction {
-  _id?: string;
-  clientCode?: string;
-  fullName?: string;
-  beneficiary?: string;
-  amount?: number;
-  date?: number;
-  type: string;
-  ncp?: string;
-  age?: string;
-  card?: {
-    code: string;
-    label: string;
-  };
-  country?: string;
-  category?: string;
-  currentMonth?: number;
+    _id?: string;
+    clientCode?: string;
+    fullName?: string;
+    beneficiary?: string;
+    amount?: number;
+    date?: number;
+    type: string;
+    ncp?: string;
+    age?: string;
+    card?: {
+        code: string;
+        label: string;
+    };
+    country?: string;
+    category?: string;
+    currentMonth?: number;
 }
 export interface VisaCeiling {
-  _id: string;
-  type: number;
-  value: number;
-  description: string;
-  date: {
-    created?: number;
-    updated?: number;
-  };
-  user?: {
     _id: string;
-    fullName: string;
-  };
+    type: number;
+    value: number;
+    description: string;
+    date: {
+        created?: number;
+        updated?: number;
+    };
+    user?: {
+        _id: string;
+        fullName: string;
+    };
+}
+
+export interface Travel {
+    _id?: string;
+    fullName?: string;
+    clientCode?: string;
+    userId?: string;
+    dates?: {
+        start: number;
+        end?: number;
+        created: number;
+        updated?: number;
+    };
+    continents?: string[];
+    countries?: {
+        name: string;
+        continent: string;
+        currency?: string;
+    }[];
+    status: OperationStatus;
+    comment?: string;
+    signature?: string;
+    travelReason?: {
+        code: string;
+        label: string;
+        otherLabel?: string;
+    };
+    travelAttachments?: Attachement[];
+    expenseDetails?: {
+        type?: OperationType;
+        date?: number;
+        currency?: string;
+        amount?: number;
+        reason?: string;
+        expenseAttachements?: Attachement[];
+    }[];
+    transaction?: VisaTransaction[];
+}
+
+
+export interface OnlinePayment {
+    _id?: string;
+    clientCode?: string;
+    userId?: string;
+    dates?: {
+        created: number;
+        updated?: number;
+    };
+    status: OperationStatus;
+    currentMonth?: number;
+    transactions?: VisaTransaction[];
+    statements?: OnlinePaymentStatement[];
+}
+
+export interface OnlinePaymentStatement {
+    fullName?: string;
+    date?: number;
+    account?: {
+        ncp: number;
+        age: number;
+    }
+    comment?: string;
+    signature?: string;
+    attachments?: Attachement[];
+}
+
+export interface Chat {
+    _id?: string;
+    operationId?: string;
+    type?: number;  // 100: travel, 200: online payment;
+    messages?: ChatMessage[];
+}
+
+export interface ChatMessage {
+    type?: string;
+    message?: string;
+    reply?: string;
+    sender?: string;
+    date?: string;
+    files?: any[];
+    quote?: string;
+    avatar?: string;
 }
 
