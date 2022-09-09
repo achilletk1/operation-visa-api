584cefca0b452134f9cd41176a2a89376648b067
achille <achille.tachum@londo.io>
2022-09-09 09:48:11 +0100

    review reporting

--- src/controllers/property-and-services-types.controller.ts
+++ src/controllers/property-and-services-types.controller.ts
@@ -43,6 +43,24 @@ export const propertyAndServicesTypesController = {
             res.status(200).json(data);
         });
 
+        app.get('/properties-and-services-all', async (req: Request, res: Response) => {
+            const data = await propertyAndServicesTypesService.getPropertyAndServicesTypesBy(req.query);
+
+            if (data instanceof Error && data.message === 'Forbbiden') {
+                const message = 'forbbiden to get data';
+                const errResp = commonService.generateErrResponse(message, data);
+                return res.status(403).json(errResp);
+            }
+
+            if (data instanceof Error) {
+                const message = 'unable to get transaction file';
+                const errResp = commonService.generateErrResponse(message, data);
+                return res.status(500).json(errResp);
+            }
+
+            res.status(200).json(data);
+        });
+
         app.get('/properties-and-services/:id', async (req: Request, res: Response) => {
             const data = await propertyAndServicesTypesService.getPropertyAndServicesTypesById(req.params.id);
 
