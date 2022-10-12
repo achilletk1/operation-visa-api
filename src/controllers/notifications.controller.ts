ea3fb2778683f603be3285ee9255c3c1920ef36f
Chri$ Samory <samory.takougne@londo-tech.com>
2022-10-12 07:50:49 -0300

    Add notification on not subscribe api

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -148,5 +148,23 @@ export const notificationsController = {
 
             res.status(200).json(data);
         });
+
+        app.get('/visa-operations/not-customer/notifications', async (req: Request, res: Response) => {
+            const data = await notificationService.getNotifications(req.query);
+
+            if (data instanceof Error && data.message === 'Forbbiden') {
+                const message = 'forbbiden to get data';
+                const errResp = commonService.generateErrResponse(message, data);
+                return res.status(403).json(errResp);
+            }
+
+            if (data instanceof Error) {
+                const message = 'unable to get transaction file';
+                const errResp = commonService.generateErrResponse(message, data);
+                return res.status(500).json(errResp);
+            }
+
+            res.status(200).json(data);
+        });
     }
 };
