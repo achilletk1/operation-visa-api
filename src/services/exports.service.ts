102d39c557e97047753d2819c5c43e9168fea805
Chri$ Samory <samory.takougne@londo-tech.com>
2022-10-17 06:18:06 -0300

    Export notification

--- src/services/exports.service.ts
+++ src/services/exports.service.ts
@@ -41,54 +41,53 @@ export const exportsService = {
     },
 
     generateNotificationExportData: async (id: string, code: any) => {
-
-        let options;
-
         try {
-            options = decode(code);
-        } catch (error) { return new Error('BadExportCode'); }
+            let options;
 
-        const { format, query , userId, ttl } = options;
+            try {
+                options = decode(code);
+            } catch (error) { return new Error('BadExportCode'); }
+    
+            const { format, query , userId, ttl } = options;
+            if ((new Date()).getTime() >= ttl) { return new Error('ExportLinkExpired'); }
+    
+            if (id !== userId) { return new Error('Forbbiden'); }
+    
+            const user = await usersService.getUserById(userId);
+    
+            if (!user) { return new Error('UserNotFound'); }
+            commonService.parseNumberFields(query);
+            const { offset, limit, start, end } = query;
+            delete query.offset;
+            delete query.limit;
+            delete query.start;
+            delete query.end;
+    
+            const range = (start && end) ? { start: moment(start, 'DD-MM-YYYY').startOf('day').valueOf(), end: moment(end, 'DD-MM-YYYY').endOf('day').valueOf() } :
+                undefined;
+    
+            const {data} =  await notificationsCollection.getNotifications(query || {}, offset || 1, limit || 40, range);
+    
+            if (!data || isEmpty(data)) {
+                logger.info('notification not found', `${classPath}.getNotifications()`);
+                return new Error('NotificationNotFound');
+            }
+    
+            let result: any;
+    
+            if (format === 'pdf') {
+                const pdfString = await exportsHelper.generateNotificationExportPdf(user, data, start, end );
+                const buffer = Buffer.from(pdfString, 'base64');
+                result = { contentType: 'application/pdf', fileContent: buffer };
+            }
+    
+            return result;
 
-        if ((new Date()).getTime() >= ttl) { return new Error('ExportLinkExpired'); }
-
-        if (id !== userId) { return new Error('Forbbiden'); }
-
-        const user = await usersService.getUserById(userId);
-
-        if (!user) { return new Error('UserNotFound'); }
-        commonService.parseNumberFields(query);
-        const { offset, limit, start, end } = query;
-        delete query.offset;
-        delete query.limit;
-        delete query.start;
-        delete query.end;
-
-        const range = (start && end) ? { start: moment(start, 'DD-MM-YYYY').startOf('day').valueOf(), end: moment(end, 'DD-MM-YYYY').endOf('day').valueOf() } :
-            undefined;
-
-        const {data} =  await notificationsCollection.getNotifications(query || {}, offset || 1, limit || 40, range);
-
-        if (!data || isEmpty(data)) {
-            logger.info('notification not found', `${classPath}.getNotifications()`);
-            return new Error('NotificationNotFound');
+        } catch (error) {
+            logger.error(`\nError export PDF \n${error.message}\n${error.stack}\n`);
+            return error;
         }
-
-        let result: any;
-
-        if (format === 'pdf') {//
-            const pdfString = await exportsHelper.generateNotificationExportPdf(user, data, start, end );
-            const buffer = Buffer.from(pdfString, 'base64');
-            result = { contentType: 'application/pdf', fileContent: buffer };
-        }
-
-        if (format === 'xlsx') {
-            const excelData = exportsHelper.generateTransactionExportXlsx(data);
-            const buffer = Buffer.from(`${excelData}`, 'base64');
-            result = { contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', fileContent: buffer };
-        }
-
-        return result;
+      
     }
 
     
