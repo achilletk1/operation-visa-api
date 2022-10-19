4141d308b498c16b6ead8cb04d4c790513671df9
Chri$ Samory <samory.takougne@londo-tech.com>
2022-10-19 14:55:55 -0300

    Setting parameters in backoffice

---
+++ scripts/helpers/insert-default-settings.ts
@@ -0,0 +1,24 @@
+import * as moment from 'moment'
+import { getDatabase } from '../config';
+
+export const inserDefaultSetting = async () => {
+
+    console.log('----------------------------------------');
+    console.log('-------  INSERT DEFAULTS SETTINGS ------');
+    console.log('----------------------------------------');
+
+    const db = await getDatabase();
+
+    const Settings = [        {
+            key: 'max_upload_file_size',
+            maxUpladFileSize: 5,
+            dateCreated: moment().valueOf(),
+            dateUpdated: [ ],
+            users: []
+        },
+    ]
+    console.log('insert default Settings  collection');   
+    db.dropCollection("settings",function(err, result) { console.log("Collection droped");});
+    const response = await db.collection('settings').insertMany(Settings);
+    console.log(response.insertedIds);
+};
