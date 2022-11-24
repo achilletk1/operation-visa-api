f6d5c99dc39502de5c24cfe1f39a92bdaad49055
armelnzogue <kevinmoutassi@MacBook-Pro-de-Kevin.local>
2022-11-24 17:19:58 +0100

    add scripts visa ceiling

---
+++ scripts/helpers/insert-default-ceiling.ts
@@ -0,0 +1,50 @@
+import * as moment from 'moment'
+import { getDatabase } from '../config';
+
+export const inserDefaultVisaCeilings = async () => {
+
+    console.log('----------------------------------------');
+    console.log('-------  INSERT DEFAULTS SETTINGS ------');
+    console.log('----------------------------------------');
+
+    const db = await getDatabase();
+
+    const ceilings = [
+        {
+            type: 100,
+            value: 5000000,
+            description: 'Voyages courtes durées (PAIEMENT TPE & RETRAIT GAB)',
+            date: {
+                created: moment().valueOf()
+            }
+        },
+        {
+            type: 200,
+            value: 1000000,
+            description: 'Paiement en ligne ou élèctronique',
+            date: {
+                created: moment().valueOf()
+            }
+        },
+        {
+            type: 300,
+            value: 5000000,
+            description: 'Voyages longues durées (PAIEMENT TPE, RETRAIT GAB, paiement élèctronique)',
+            date: {
+                created: moment().valueOf()
+            }
+        },
+        {
+            type: 400,
+            value: 2000000,
+            description: 'Plafond pour les étudiants lors des voyages courtes et longues durées',
+            date: {
+                created: moment().valueOf()
+            }
+        },
+    ]
+    console.log('insert default ceilings  collection');
+    db.dropCollection("settings", function (err, result) { console.log("Collection droped"); });
+    const response = await db.collection('visa_transactions_ceillings').insertMany(ceilings);
+    console.log(response.insertedIds);
+};
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,12 +3,14 @@ import * as Confirm from 'prompt-confirm';
 import { config } from './config-env';
 import { inserDefaultLongTravelsTypes } from './helpers/insert-default-long-travels-types';
 import { inserDefaultSetting } from './helpers/insert-default-settings';
+import { inserDefaultVisaCeilings } from './helpers/insert-default-ceiling';
 
 const runScripts = async () => {
     // SCRIPTS to execute
    await inserDefaultLongTravelsTypes();
    await inserDefaultVouchers();
    await inserDefaultSetting();
+   await inserDefaultVisaCeilings();
 }
 
 if (config.get('env') !== 'development') {
