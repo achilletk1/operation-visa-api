42e57cc2e0c4e994d4e2046a7b39d4167115cbe0
kevin moutassi <kevin.moutassi@londo-tech.com>
2023-08-25 13:55:46 +0100

    armel modif

--- src/index.ts
+++ src/index.ts
@@ -91,8 +91,10 @@ server.listen(config.get('port'), config.get('host'), async () => {
     logger.info(`server started. Listening on port ${config.get('port')} in "${config.get('env')}" mode`);
 });
 cronService.startRemoveOnpWithoutExceeding();
-cronService.detectUsersNotJustifiedTransaction();
 cronService.startTransactionsProcessing();
+cronService.detectUsersNotJustifiedTransaction();
+cronService.detectListOfUsersToBlocked();
+cronService.startRemoveTemporaryFiles();
 cronService.startRevivalMail();
 
 export default app;
