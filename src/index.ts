ecd347ef9c905ea567b43ee74ee8483c54d27889
Dimitri Signe <dimitri.signe@londo-tech.com>
2023-10-17 12:16:48 +0100

    comment operation visa cron

--- src/index.ts
+++ src/index.ts
@@ -91,11 +91,11 @@ server.listen(config.get('port'), config.get('host'), async () => {
     logger.info(`server started. Listening on port ${config.get('port')} in "${config.get('env')}" mode`);
 });
 
-cronService.instantiate();
-cronService.startRemoveOnpWithoutExceeding();
-cronService.startTransactionsProcessing();
-cronService.detectListOfUsersToBlocked();
-cronService.startRemoveTemporaryFiles();
-cronService.startRevivalMail();
+// cronService.instantiate();
+// cronService.startRemoveOnpWithoutExceeding();
+// cronService.startTransactionsProcessing();
+// cronService.detectListOfUsersToBlocked();
+// cronService.startRemoveTemporaryFiles();
+// cronService.startRevivalMail();
 
 export default app;
