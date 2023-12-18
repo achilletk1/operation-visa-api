f02482b16999b8e80fedc3883aa1a1f3b68fffac
Salam Sahane <sahane.salam@londo.io>
2023-12-18 19:05:09 +0100

    review of authentication process

--- src/modules/cbs/oracle-daos/helpers/clients.dao.helper.ts
+++ src/modules/cbs/oracle-daos/helpers/clients.dao.helper.ts
@@ -319,7 +319,7 @@ export const helper = {
                 NRC: 'GWB5O3RPI4WWUCJ/DV    ',
                 NIDF: 'TGUZF1UGDT9KAQ   ',
                 AGE: '01400   ',
-                NCP: '37207027067',
+                NCP: '37207027066',
                 CLC: '19 ',
                 CIVILITY: 'Camerounaise',
                 ADDRESS: 'Ange rafael ',
