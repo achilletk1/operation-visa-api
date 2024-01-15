6f099c1284bfef04f5825858bdc9ac2b6fddaab5
steve lebada <dominique.mvogo@londo-tech.com>
2024-01-15 14:20:13 +0100

    crud card type in api

---
+++ src/modules/card-type/card-type.repository.ts
@@ -0,0 +1,7 @@
+import { BaseRepository } from "common/base";
+
+export class CardTypeRepository extends BaseRepository {
+
+    constructor() { super(); this.collectionName = 'visa_operations_card_type'; }
+
+}
---
+++ src/modules/card-type/card-type.route.ts
@@ -0,0 +1,17 @@
+import { CardTypeController } from './card-type.controller';
+import express from 'express';
+
+const router = express.Router();
+export const cardTypeController = new CardTypeController();
+
+router.get('/', cardTypeController.findAll);
+
+router.get('/all', cardTypeController.findAll);
+
+router.get('/:id', cardTypeController.findOneById);
+
+router.post('/', cardTypeController.insertCardType);
+
+router.put('/:id', cardTypeController.updateById);
+
+export const cardTypeRoute = router;
