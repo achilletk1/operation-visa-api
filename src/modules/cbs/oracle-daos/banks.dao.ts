ed4c8ec09a7667069f9a1e8d082838d4f1cff3eb
achille <achille.tachum@londo.io>
2024-01-22 09:46:26 +0100

    change prefix bank to infoc

--- src/modules/cbs/oracle-daos/banks.dao.ts
+++ src/modules/cbs/oracle-daos/banks.dao.ts
@@ -18,13 +18,13 @@ export const banksDAO = {
     
             const query = `
             select
-                distinct pays, lib_pays, code_banque, nom_banque, code_agence, (select b.nom from bank.bkbqe b where b.etab = a.code_banque and b.guib = a.code_agence and rownum = 1) nom_agence
+                distinct pays, lib_pays, code_banque, nom_banque, code_agence, (select b.nom from infoc.bkbqe b where b.etab = a.code_banque and b.guib = a.code_agence and rownum = 1) nom_agence
             from
                 (
                     select
-                        a.pays, a.etap code_banque, a.guip code_agence, (select lib1 from bank.bknom where ctab = '033' and cacc = a.pays ) lib_pays,
-                        (select lib1 from bank.bknom where ctab = '840' and age = '99500' and cacc = a.etap) nom_banque
-                    from bank.bkbqe a
+                        a.pays, a.etap code_banque, a.guip code_agence, (select lib1 from infoc.bknom where ctab = '033' and cacc = a.pays ) lib_pays,
+                        (select lib1 from infoc.bknom where ctab = '840' and age = '99500' and cacc = a.etap) nom_banque
+                    from infoc.bkbqe a
                     where comp = 'O' and etap not in ('20001','91001','92001','96001')
                 ) a
             where trim(nom_banque) is not null and trim(lib_pays) is not null and substr(pays,1,3) || substr(code_banque,1,1) not in ('1781','1782','1784','1785','1786')
